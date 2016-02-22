e68776df5797adf5f69353ac8dc981ff3b9540bc
Florian Moser <git@famoser.ch>
2016-02-22 23:37:33 +0100

    can add enw note by clicking enter

--- Famoser.RememberLess.Presentation.WindowsUniversal/Pages/MainPage.xaml.cs
+++ Famoser.RememberLess.Presentation.WindowsUniversal/Pages/MainPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using Windows.Foundation.Metadata;
+using Windows.System;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml.Controls;
 using Famoser.RememberLess.View.Enums;
+using Famoser.RememberLess.View.ViewModel;
 using GalaSoft.MvvmLight.Messaging;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -22,6 +24,16 @@ namespace Famoser.RememberLess.Presentation.WindowsUniversal.Pages
         private void Page_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             Messenger.Default.Send(Messages.SyncNotes);
+        }
+
+        private void TextBox_KeyDown(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
+        {
+            if (e.Key == VirtualKey.Enter)
+            {
+                var vm = DataContext as MainViewModel;
+                if (vm?.AddNoteCommand.CanExecute(null) == true)
+                    vm.AddNoteCommand.Execute(null);
+            }
         }
     }
 }
